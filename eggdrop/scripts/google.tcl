150b734404afac7d5d4e7116f7a793a31e900d85
Juan Felipe Alvarez Saldarriaga <jfalvarez@vesifront.org>
2009-08-09 14:31:42 -0500

    Added eggdrop scripts made with tcl. One is to search on google, support for define: is added, and the other one is to search into php documentation and to search functions.

---
+++ eggdrop/scripts/google.tcl
@@ -0,0 +1,150 @@
+#
+# Google Scraper
+#
+# Juan Felipe Alvarez Saldarriaga <jfalvarez at vesifront dot org>
+
+# Max results.
+set google_config(max) 5
+
+# Deny channels
+set google_config(deny_chans) {
+}
+
+# Version
+set google_config(ver) 0.1
+
+# Include packages.
+package require http
+package require ncgi
+
+# Public command.
+bind pub - !google pub_google
+
+# Google function.
+proc pub_google { nick uhost hand chan arg } {
+	global google_config
+
+	if { [lsearch [string tolower $google_config(deny_chans)] [string tolower $chan]] != "-1" } {
+    	return 0
+  	}
+
+  	if { $arg == "" } {
+    	putserv "NOTICE $nick :Usage: !google <search terms>"
+	    return 0
+  	}
+
+	search $chan $nick $arg
+}
+
+# Search on google and display results.
+proc search { chan nick search } {
+	global google_config
+
+	regsub -all " " $search "+" search
+
+	# Set result type.
+	set extra ""
+	set type search
+
+	if { [string match -nocase define:* $search] } {
+		set extra &oi=glossary_definition
+		set type dict
+		regsub {[Dd]efine:} $search {define:} search
+	}
+
+	# Set google search url.
+	set url http://www.google.com/search?q=$search&num=$google_config(max)$extra
+
+	# Connect to google.
+	http::config -useragent "Mozilla/5.0"
+    set token [http::geturl $url]
+    set data [http::data $token]
+    http::cleanup $token
+
+	# Create a temp file with the page html string.
+    set tmp_file "/tmp/tmp_file_[expr int(rand()*32768)].[pid].xml"
+    set fp [open $tmp_file "w"]
+    puts -nonewline $fp $data
+    close $fp
+
+	# Parse the temp file with tity.
+	set tidy_command "tidy -qm -w 0 -utf8 -asxml $tmp_file 2> /dev/null"
+	catch { eval exec $tidy_command }
+
+    set fp [open $tmp_file "r"]
+    set tidy [read $fp]
+    set tidy [split $tidy "\n"]
+    catch { close $fp }
+
+    # file delete -force -- $tmp_file
+
+	switch $type {
+		search {
+			set results ""
+			set links_counter 1
+
+			foreach row $tidy {
+				if { [regexp -nocase -- {Your search.*did not match any documents} $row] } {
+					putserv "PRIVMSG $chan :No results"
+					return 0
+				}
+
+				
+				if { [regexp -nocase -- {<a.*href=.*class="l"} $row] } {
+					regexp -nocase -- {.*<a href="(.*)" class="l">(.*)</a>.*} $row all link text
+					set link [ncgi::decode $link]
+					set text [striptags [ncgi::decode $text]]
+
+            		set title [format "%u. %s" $links_counter $text]
+
+					if { $links_counter == 1  } {
+						set public_result "$title <$link>";
+					}
+
+					set results "$results $title <$link>;"
+
+					incr links_counter
+				}
+			}
+
+			putserv "PRIVMSG $chan :$nick: $public_result"
+
+			if { $links_counter > 1 } {
+				putserv "NOTICE $nick :$results"
+			}
+		}
+		dict {
+			set def_counter 1
+
+			foreach row $tidy {
+				if { [regexp -nocase -- {No definitions were found for.*} $row] } {
+					putserv "PRIVMSG $chan :No definition"
+					return 0
+				}
+
+				if { [regexp -nocase -- {<li>.*} $row] && $def_counter == 1 } {
+					regexp -nocase -- {<li>(.*)<br />.*} $row all text
+					incr def_counter
+				}
+
+				if { [regexp -nocase -- {<a href=\"/url\?&amp;q=.*} $row] && $def_counter == 2 } {
+					regexp -nocase -- {<a href=\"/url\?&amp;q=(.*)(&amp;)ei=.*} $row all link
+					set link [ncgi::decode $link]
+					break
+				}
+			}
+
+			putserv "PRIVMSG $chan :$nick: $text <$link>"
+		}
+	}
+}
+
+proc striptags str {
+    regsub -all {<.*?>} $str {} str
+    return $str
+}
+
+
+# Loaded.
+putlog "google.tcl \[ OK \]"
+putlog "Google Wrapper $google_config(ver) - Loaded"
---
+++ eggdrop/scripts/php.tcl
@@ -0,0 +1,181 @@
+# Max results.
+set php_config(max) 5
+
+# Deny channels
+set php_config(deny_chans) {
+}
+
+# Version
+set php_config(ver) 0.1
+
+# Include packages.
+package require http
+package require ncgi
+
+# Public command.
+bind pub - !php pub_php_docs
+bind pub - !function pub_php_function
+
+# Search into the php docs.
+proc pub_php_docs { nick uhost hand chan arg } {
+	global php_config
+
+	if { [lsearch [string tolower $php_config(deny_chans)] [string tolower $chan]] != "-1" } {
+    	return 0
+  	}
+
+  	if { $arg == "" } {
+    	putserv "NOTICE $nick :Usage: !php <search terms>"
+	    return 0
+  	}
+
+	search $chan $nick $arg
+}
+
+# Search into online docs.
+proc search { chan nick search } {
+	global php_config
+
+	regsub -all " " $search "+" search
+
+	set url http://www.php.net/results.php?q=$search&p=manual&l=en
+
+	set tidy [process_request $url]
+
+	set results ""
+	set links_counter 1
+
+	foreach row $tidy {
+		if { [regexp -nocase -- {No pages matched your query} $row] } {
+			putserv "PRIVMSG $chan :No results"
+			return 0
+		}
+
+		if { [regexp -nocase -- {<p class="result"} $row] } {
+			regexp -nocase -- {.*<a href="(.*)".*>(.*)</a>.*} $row all link text
+
+			if { [string match -nocase "http://www.php.net*" $link] } {
+				set link [ncgi::decode $link]
+			} else {
+				set link "http://www.php.net[ncgi::decode $link]"
+			}
+
+			set text [striptags [ncgi::decode $text]]
+			set title [format "%u. %s" $links_counter $text]
+
+			if { $links_counter == 1  } {
+				set public_result "$title <$link>";
+			}
+
+			set results "$results $title <$link>;"
+
+			if { $links_counter == $php_config(max) } {
+				break;
+			}
+			
+			incr links_counter
+		}
+	}
+
+	putserv "PRIVMSG $chan :$nick: $public_result"
+
+	if { $links_counter > 1 } {
+		putserv "NOTICE $nick :$results"
+	}
+}
+
+# Look for a php function.
+proc pub_php_function { nick uhost hand chan arg } {
+	global php_config
+
+	if { [lsearch [string tolower $php_config(deny_chans)] [string tolower $chan]] != "-1" } {
+    	return 0
+  	}
+
+  	if { $arg == "" } {
+    	putserv "NOTICE $nick :Usage: !function <function name>"
+	    return 0
+  	}
+
+	function $chan $nick $arg
+}
+
+# Process function page.
+proc function { chan nick search } {
+	regsub -all " " $search "+" search
+
+	set url http://www.php.net/manual/en/function.$search.php
+
+	set tidy [process_request $url]
+
+	# Flag to know if we have something here.
+	set name ""
+
+	foreach row $tidy {
+		# Get function name.
+		if { [regexp -nocase -- {<h1 class=\"refname\">.*} $row] } {
+			regexp -nocase -- {<h1 class=\"refname\">(.*)</h1>} $row all name
+		}
+
+		# Get version info.
+		if { [regexp -nocase -- {<p class=\"verinfo\">.*} $row] } {
+			regexp -nocase -- {<p class=\"verinfo\">(.*)</p>} $row all version
+		}
+
+		# Get description.
+		if { [regexp -nocase -- {<span class=\"dc-title\">.*} $row] } {
+			regexp -nocase -- {<span class=\"dc-title\">(.*)</span>} $row all desc 
+		}
+
+		# Get params.
+		if { [regexp -nocase -- {<div class=\"methodsynopsis dc-description\">.*} $row] } {
+			regexp -nocase -- {<div class=\"methodsynopsis dc-description\">(.*)} $row all params
+			set params [striptags $params]
+		}
+	}
+
+	if { $name != "" } {
+		putserv "PRIVMSG $chan :\002$name\002 $version - $desc"
+		putserv "PRIVMSG $chan :$params"
+	} else {
+		putserv "PRIVMSG $chan :That function is not in the online manual"
+	}
+}
+
+proc striptags str {
+    regsub -all {<.*?>} $str {} str
+    return $str
+}
+
+# Process request
+proc process_request { url } {
+	# Get data.
+	http::config -useragent "Mozilla/5.0"
+    set token [http::geturl $url]
+    set data [http::data $token]
+    http::cleanup $token
+
+	# Create a temp file with the page html string.
+    set tmp_file "/tmp/tmp_file_[expr int(rand()*32768)].[pid].xml"
+    set fp [open $tmp_file "w"]
+    puts -nonewline $fp $data
+    close $fp
+
+	# Parse the temp file with tity.
+	set tidy_command "tidy -qm -w 0 -utf8 -asxml $tmp_file 2> /dev/null"
+	catch { eval exec $tidy_command }
+
+    set fp [open $tmp_file "r"]
+    set tidy [read $fp]
+    set tidy [split $tidy "\n"]
+    catch { close $fp }
+
+    file delete -force -- $tmp_file
+
+	return $tidy
+}
+
+
+# Loaded.
+putlog "php.tcl \[ OK \]"
+putlog "PHP Wrapper $php_config(ver) - Loaded"
